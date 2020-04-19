43c540270b1eca755d2f1739422497185797431e
nyow <itismenishaanth@gmail.com>
2020-04-19 20:20:52 +0530

    Add Client to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "../Header/Header";
 import Hero from "../Hero/Hero";
+import Client from "../Client/Client";
 
 function App() {
   return (
     <section>
       <Header />
       <Hero />
+      <Client />
     </section>
   );
 }
