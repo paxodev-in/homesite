aa71733551354a9626b17c45785e9e74ca20f33e
nyow <itismenishaanth@gmail.com>
2020-04-19 20:15:10 +0530

    Revert "Add Header to App"
    This reverts commit fd5db1166c9cb3868cdb6ba0d60a8a818b751faf.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
 import Header from "../Header/Header";
-import Hero from "../Hero/Hero.js";
 
 function App() {
   return (
-    <section>
-      <Header />
-      <Hero />
-    </section>
+    <Header />
   );
 }
 
