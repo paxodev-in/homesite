fd5db1166c9cb3868cdb6ba0d60a8a818b751faf
nyow <itismenishaanth@gmail.com>
2020-04-19 20:10:30 +0530

    Add Header to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from "../Header/Header";
+import Hero from "../Hero/Hero.js";
 
 function App() {
   return (
-    <Header />
+    <section>
+      <Header />
+      <Hero />
+    </section>
   );
 }
 
