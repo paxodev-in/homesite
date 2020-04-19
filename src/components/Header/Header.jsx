60851cd2587125c91735893bfeb26d0f8d965dca
nyow <itismenishaanth@gmail.com>
2020-04-19 19:55:30 +0530

    Add container to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function Header() {
     return(
         <header>
-            <nav>
-                <div class="nav-wrapper">
-                    <a href="#" class="brand-logo">Paxodev</a>
-                    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <nav className="container">
+                <div className="nav-wrapper">
+                    <a href="#" className="brand-logo">Paxodev</a>
+                    <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
                             <li><a href="sass.html">Home</a></li>
                             <li><a href="badges.html">About</a></li>
                             <li><a href="collapsible.html">Contact</a></li>
                         </ul>
-                        <ul class="sidenav" id="mobile-demo">
+                        <ul className="sidenav" id="mobile-demo">
                             <li><a href="sass.html">Sass</a></li>
                             <li><a href="badges.html">Components</a></li>
                             <li><a href="collapsible.html">Javascript</a></li>
@@ -23,5 +23,6 @@ function Header() {
         </header>
     );
 }
+
 
 export default Header;
