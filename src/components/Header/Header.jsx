f6f898697bec53cde1836429ee702fed242e8529
Andrew2432 <drew.devops@gmail.com>
2020-05-01 22:01:44 +0530

    Add styling to header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,32 @@
-import React from "react";
+import React from 'react';
 
-function Header() {
-    return(
-        <header>
-            <nav>
-                <div className="nav-wrapper">
-                    <a href="#" className="brand-logo">Paxodev</a>
-                    <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-                        <ul id="nav-mobile" className="right hide-on-med-and-down">
-                            <li><a href="sass.html">Home</a></li>
-                            <li><a href="badges.html">About</a></li>
-                            <li><a href="collapsible.html">Contact</a></li>
-                        </ul>
-                        <ul className="sidenav" id="mobile-demo">
-                            <li><a href="sass.html">Sass</a></li>
-                            <li><a href="badges.html">Components</a></li>
-                            <li><a href="collapsible.html">Javascript</a></li>
-                            <li><a href="mobile.html">Mobile</a></li>
-                         </ul>
-                    </div>
-            </nav>
-        </header>
-    );
-}
-
+const Header = () => {
+  return (
+    <header>
+      <nav className={`navbar bg-primary`}>
+        <div className="navbar-brand">
+          <h2>
+            <a href="/">Paxodev</a>
+          </h2>
+        </div>
+        <div className="navbar-links">
+          <ul className="navbar-links-items">
+            <li>
+              <a href="/" className="active">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
